29908573021dd188a872bef685ccfafdb409d40f
Juliana Rodrigues <juliana.rodrigues-29@hotmail.com>
2026-02-10 16:45:54 +0100

    corrige o evento?

--- content/eventos/events.11tydata.js
+++ content/eventos/events.11tydata.js
@@ -1,5 +1,7 @@
+import slugify from "slugify";
+
 export default {
     layout: "layouts/evento.njk",
-    permalink: data => `/eventos/${data.title.toLowerCase().replace(/ /g,"-")}/`,
+    permalink: data => `/eventos/${slugify(data.title, { lower: true, strict: true })}/`,
     eleventyExcludeFromCollections: false,
 };
