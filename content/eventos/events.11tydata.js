27833749b7e031a461a196094ad10b5786f10a6e
Juliana Rodrigues <juliana.rodrigues-29@hotmail.com>
2026-02-10 18:21:57 +0100

    corrigindo js de evento?

--- content/eventos/events.11tydata.js
+++ content/eventos/events.11tydata.js
@@ -1,4 +1,7 @@
 export default {
     layout: "layouts/event.njk",
     tags: ["events"],
+    eleventyComputed: {
+        content: data => data.body, // mapeia o body do PageCMS para content do Eleventy
+    }
 };
