7e6060e51684bcd89f2a5227e10a206c3c75d11d
Juliana Rodrigues <juliana.rodrigues-29@hotmail.com>
2026-02-10 17:33:57 +0100

    novo teste para eventos

--- content/eventos/events.11tydata.js
+++ content/eventos/events.11tydata.js
@@ -1,4 +1,5 @@
 export default {
-    layout: "layouts/evento.njk",
-    tags: ["events"],
+    layout: "layouts/evento.njk",  // garante que cada evento use esse layout
+    permalink: data => `/eventos/${data.title.toLowerCase().replace(/ /g, "-")}/`,
+    eleventyExcludeFromCollections: false
 };
