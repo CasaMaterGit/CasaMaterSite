82ecf3b7488159087405d5b384215167ba732b6c
Juliana Rodrigues <juliana.rodrigues-29@hotmail.com>
2026-02-10 17:25:35 +0100

    adiciona njk para eventos

--- content/eventos/events.11tydata.js
+++ content/eventos/events.11tydata.js
@@ -1,7 +1,4 @@
-import slugify from "slugify";
-
 export default {
     layout: "layouts/evento.njk",
-    permalink: data => `/eventos/${slugify(data.title, { lower: true, strict: true })}/`,
-    eleventyExcludeFromCollections: false,
+    tags: ["events"],
 };
