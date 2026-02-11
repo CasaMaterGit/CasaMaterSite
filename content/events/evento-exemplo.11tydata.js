6b277496142eb766cd89ccaa586557f84203b38b
Juliana Rodrigues <juliana.rodrigues-29@hotmail.com>
2026-02-11 07:54:38 +0100

    consertando eventsjs??

--- content/events/evento-exemplo.11tydata.js
+++
@@ -1,4 +0,0 @@
-export default {
-    tags: ["events"],
-    "layout": "layouts/event.njk",
-};
---
+++ content/events/events.11tydata.js
@@ -0,0 +1,5 @@
+export default {
+    tags: ["events"],
+    layout: "layouts/event.njk",
+    permalink: "/events/{{ title | slugify }}/",
+};
